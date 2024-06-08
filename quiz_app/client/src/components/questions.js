ce755fcbe965817cc03d01843b236b021a274a56
nimeshasewmini <123630982+nimeshasewmini@users.noreply.github.com>
2024-06-08 19:18:04 +0530

    questions

--- quiz_app/client/src/components/questions.js
+++ quiz_app/client/src/components/questions.js
@@ -1,25 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react';
 
-export default function questions() {
-    
-    function onSelect(){
-        console.log('radio button change')
+export default function Questions() {
+    const [checked, setChecked] = useState(false);  // or any appropriate initial value
+
+    function onSelect() {
+      
+        console.log('radio button change');
+        setChecked(!checked); // Toggle the checked state
     }
 
-  return (
-    <div className='questions'>
-      <h2 className='text-light'>Simple Question 1</h2>
-      <ul>
-        <li>
-            <input
-             type="radio"
-             value={true}
-             name="options"
-             id='q1-option'
-             onChange={onSelect()}
-             />
-        </li>
-      </ul>
-    </div>
-  )
+    return (
+        <div className='questions'>
+            <h2 className='text-light'>Simple Question 1</h2>
+            <ul>
+                <li>
+                    <input
+                        type="radio"
+                        checked={checked}
+                        name="options"
+                        id='q1-option'
+                        onChange={onSelect} // Pass the function reference
+                    />
+                </li>
+            </ul>
+        </div>
+    );
 }
